1e27be6513401536cfcfc05e86acb575e78081a2
Artem Sementsov <artem.sementsov@gmail.com>
2022-04-29 18:03:49 +0300

    init

---
+++ src/main/java/org/sementsov/atmemulator/AtmEmulatorApplication.java
@@ -0,0 +1,13 @@
+package org.sementsov.atmemulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtmEmulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AtmEmulatorApplication.class, args);
+    }
+
+}
